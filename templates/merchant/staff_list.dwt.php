d6c5fbdb00d7b2d2e7db2da35caeefc3120c29c8
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-09-21 17:29:55 +0800

    更新员工模块模板样式

--- templates/merchant/staff_list.dwt.php
+++ templates/merchant/staff_list.dwt.php
@@ -13,6 +13,12 @@
 		<h2 class="page-header">
 			<!-- {if $ur_here}{$ur_here}{/if} -->
 			<div class="pull-right">
+				{if $action_link_group}
+				<a href="{$action_link_group.href}" class="btn btn-primary data-pjax">
+					<i class="fa fa-reply"></i> {$action_link_group.text}
+				</a>
+				{/if}
+				
 				{if $action_link}
 				<a href="{$action_link.href}" class="btn btn-primary data-pjax">
 					<i class="fa fa-plus"></i> {$action_link.text}
@@ -23,46 +29,6 @@
 	</div>
 </div>
 
-<div class="row">
-	<div class="col-lg-12">
-		<div class="panel">
-	    	<div class="panel-body">
-	        	<div class="row">
-	            	<div class="col-sm-3">
-	            		 {if $Manager.avatar}
-	                	 	<img src="{RC_Upload::upload_url()}/{$Manager.avatar}" alt="头像" style="width:180px;margin-left:25px; margin-top:4px;border-radius:180px;box-shadow:0px 0px 8px #7E7E7E;"/>
-	                	 {else}
-	                	 	<img src="{$ecjia_main_static_url}img/ecjia_avatar.jpg" alt="头像"  style="width:180px;margin-left:25px; margin-top:4px;border-radius:180px;box-shadow:0px 0px 8px #7E7E7E;" /><br>
-	                	 {/if}
-	            	</div>
-	            	
-	            	<div class="col-sm-5">
-		                <h4 class="title-real-estates">
-                            <strong>{$Manager.name}</strong> <span class="text_center"><small class="label label-warning">店长</small></span>
-                        </h4>
-	                	<hr style="margin-top: 10px;margin-bottom: 10px;">
-	                	<p>{if $Manager.introduction}{$Manager.introduction}{else}店长有点懒哦，赶紧去个人中心完善资料吧~~{/if}</p>
-	                	{if $parent_id eq 0}
-	                	<p><a class="data-pjax" href='{RC_Uri::url("staff/mh_log/init", "user_id={$Manager.user_id}")}'>查看日志</a> | <a class="data-pjax" href='{RC_Uri::url("staff/merchant/edit", "user_id={$Manager.user_id}&parent_id=0")}'>编辑</a></p>
-	                	{/if}
-	            	</div>
-	            	
-	            	<div class="col-sm-4">
-		                <h4 class="title-real-estates">
-		                    <strong>店长资料</strong>
-		                </h4>
-	                	<hr style="margin-top: 10px;margin-bottom: 10px;">
-	                	<div><label>昵称：</label>{$Manager.nick_name}</div>
-	                	<div><label>手机账号：</label>{$Manager.mobile}</div>
-	                	<div><label>邮箱账号：</label>{$Manager.email}</div>
-	                	<div><label>加入时间：</label>{$Manager.add_time}</div>
-	                	<div><label>最后登录时间：</label>{$Manager.last_login}</div>
-	            	</div>
-	        	</div>
-	    	</div>
-		</div>
-	</div>
-</div>
 
 <div class="row">
 	<div class="col-lg-12">
@@ -76,9 +42,6 @@
 				</form>
 			</div>
 			<div class="panel-body panel-body-small">
-				<header class="panel-heading">
-                   <strong>员工列表</strong>（最多只能添加10个员工）
-                </header>
 				<section class="panel">
 					<table class="table table-striped table-advance table-hover">
 						<thead>
