214708ba465c0321afb16d5489c0887dec6f269d
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-11-03 11:01:50 +0800

    删除多余css js加载

--- admin_store_staff.php
+++ admin_store_staff.php
@@ -51,12 +51,10 @@ defined('IN_ECJIA') or exit('No permission resources.');
  */
 class admin_store_staff extends ecjia_admin
 {
-    private $db_region;
     public function __construct()
     {
         parent::__construct();
 
-        $this->db_region = RC_Model::model('store/region_model');
         RC_Loader::load_app_func('global', 'store');
         RC_Loader::load_app_func('merchant_store', 'store');
         assign_adminlog_content();
@@ -72,11 +70,8 @@ class admin_store_staff extends ecjia_admin
         RC_Style::enqueue_style('uniform-aristo');
         RC_Script::enqueue_script('jquery-chosen');
         RC_Style::enqueue_style('chosen');
-
         RC_Style::enqueue_style('splashy');
 
-        RC_Script::enqueue_script('jquery-range', RC_App::apps_url('statics/js/jquery.range.js', __FILE__));
-        RC_Style::enqueue_style('range', RC_App::apps_url('statics/css/range.css', __FILE__), array());
         //时间控件
         RC_Script::enqueue_script('bootstrap-datepicker', RC_Uri::admin_url('statics/lib/datepicker/bootstrap-datepicker.min.js'));
         RC_Style::enqueue_style('datepicker', RC_Uri::admin_url('statics/lib/datepicker/datepicker.css'));
