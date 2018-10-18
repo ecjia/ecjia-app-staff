203683502cd6a5b49fa771757c7d14ef2e92d697
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-10-18 11:22:22 +0800

    收银台设备登录绑定

--- modules/v2/admin/user/signin_module.class.php
+++ modules/v2/admin/user/signin_module.class.php
@@ -120,12 +120,12 @@ class v2_admin_user_signin_module extends api_admin implements api_interface {
                 $result = new ecjia_error('login_error', __('您输入的帐号信息不正确'));
                 return $result;
             }
-            //$device_udid = trim($device['udid']);
+            $device_udid = trim($device['udid']);
             //当前登录的收银设备是否是当前店铺的
-            //$cashier_device_info = RC_DB::table('cashier_device')->where('store_id', $staff_user_info['store_id'])->where('device_sn', $device_udid)->first();
-            //if (empty($cashier_device_info)) {
-            //	return new ecjia_error('cashier_device_error', __('此设备不属于当前店铺设备，请使用当前店铺设备登录！'));
-            //}
+            $cashier_device_info = RC_DB::table('cashier_device')->where('store_id', $staff_user_info['store_id'])->where('device_sn', $device_udid)->first();
+            if (empty($cashier_device_info)) {
+            	return new ecjia_error('cashier_device_error', __('此设备不属于当前店铺设备，请使用当前店铺设备登录！'));
+            }
             $username   = $staff_user_info['mobile'];
             $salt       = $staff_user_info['salt'];
         } else {
