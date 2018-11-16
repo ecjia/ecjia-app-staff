1bad43487a4a1420dbdf9c1fa4c031912dafd19b
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-11-16 23:03:42 +0800

    店铺打烊时间修改

--- classes/hooks/merchant_staff.class.php
+++ classes/hooks/merchant_staff.class.php
@@ -75,7 +75,11 @@ class merchant_staff_hooks
                     $dif_hour = 23 - $start['0'];
                     $dif_min = 60 - $start['1'];
                     
-                    $start_time = $start_time - 24*3600;
+	                $now_time_str = date('H:i');
+	                $now_time_arr = explode(':', $now_time_str);
+	                if ($now_time_arr['0'] < 12) {
+	                	$start_time = $start_time - 24*3600;
+	                }
                     $end_time = $start_time + ($dif_hour*3600 + $dif_min*60) + ($hour*3600 + $end['1'] *60);
                 }
                 if ($start_time < $current_time && $current_time < $end_time) {
