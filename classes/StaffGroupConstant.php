d50ec671ba9d37fe5c43f90cceb22a4f07a7bc47
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-01-15 11:46:27 +0800

    提交员工内置分组ID

--- classes/StaffGroupConstant.php
+++ classes/StaffGroupConstant.php
@@ -6,13 +6,30 @@
  * Time: 11:41
  */
 
-namespace Ecjia\App\Express;
+namespace Ecjia\App\Staff;
 
 
-class ExpressConstant
+class StaffGroupConstant
 {
+    /**
+     * 配送员默认分组ID
+     */
+    const GROUP_EXPRESS = -1;
 
-    const GROUP_S;
+    /**
+     * 收银员默认分组ID
+     */
+    const GROUP_CASHIER = -2;
+
+    /**
+     * 客服人员默认分组ID
+     */
+    const GROUP_CUSTOMERSERVICE = -3;
+
+    /**
+     * 代理商默认分组ID
+     */
+    const GROUP_AGENT = -21;
 
 
 
