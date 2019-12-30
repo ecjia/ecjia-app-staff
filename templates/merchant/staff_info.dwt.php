c9bcf325d9305cf4d1d7a03c9fa4145a72df045c
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-12-30 15:48:31 +0800

    修复员工添加分组问题

--- templates/merchant/staff_info.dwt.php
+++ templates/merchant/staff_info.dwt.php
@@ -92,7 +92,7 @@
                         <div class="col-lg-6 controls">
                             <input class="form-control" id="name" name="name" type="text" />
                         </div>
-                        <span class="input-must m_l7"><span class="require-field" style="color:#FF0000,">*</span></span>
+                        <span class="input-must m_l7"><span class="require-field" style="color:#FF0000">*</span></span>
                     </div>
 
                     <div class="form-group">
@@ -141,7 +141,7 @@
                         <div class="col-lg-6 controls">
                               <input class="form-control" id="email" name="email" type="email" />
                         </div>
-                        <span class="input-must m_l7"><span class="require-field" style="color:#FF0000,">*</span></span>
+                        <span class="input-must m_l7"><span class="require-field" style="color:#FF0000">*</span></span>
                     </div>
                     
                     <div class="form-group">
@@ -149,7 +149,7 @@
                         <div class="col-lg-6 controls">
                               <input class="form-control" type="password" id="password" name="password"  />
                         </div>
-                        <span class="input-must m_l7"><span class="require-field" style="color:#FF0000,">*</span></span>
+                        <span class="input-must m_l7"><span class="require-field" style="color:#FF0000">*</span></span>
                     </div>
                       
                     <div class="form-group">
@@ -157,7 +157,7 @@
                          <div class="col-lg-6 controls">
                               <input class="form-control" id="confirm_password" name="confirm_password" type="password" />
                           </div>
-                        	<span class="input-must m_l7"><span class="require-field" style="color:#FF0000,">*</span></span>
+                        	<span class="input-must m_l7"><span class="require-field" style="color:#FF0000">*</span></span>
                     </div>
                     
                     <div class="form-group ">
